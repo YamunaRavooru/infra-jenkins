cf89341f3507bb8aa990e82c67915999d9cbabb3
yamuna <malini.mahathi@gmail.com>
2025-05-28 13:07:15 +0530

    frontend

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3"{
     bucket = "malini-remote-tfstate-dev"
-    key    = "expense-dev-eks-bastion" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    key    = "expense-dev-bastion" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "malini-remote-tfstate-dev"
 }
